6922524ff0a57559877dbb090c3f0361b33af89c
Rovshan Azimov <azi.rovshan>
2022-06-07 15:13:07 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,103 @@
+document.addEventListener('touchstart', handleTouchStart, false);
+document.addEventListener('touchmove', handleTouchMove, false);
+const popupOpen = document.querySelector('.third-container__btn');
+const popupClose = document.querySelector('.popup__btn');
+const popupContainer = document.querySelector('.popup');
+const containersLine = document.querySelector('.containers__line');
+const logo = document.querySelectorAll('.logo');
+const firstContainerButton = document.querySelector('.first-container__btn');
+const popupLine = document.querySelector('.popup__bodies');
+const pag1 = document.querySelector('.pag-1');
+const pag2 = document.querySelector('.pag-2');
+
+let offsetThird = 0;
+let offset = 0;
+
+document.querySelector('.popup__next').addEventListener('click', function () {
+  offsetThird = offsetThird + 450;
+  if (offsetThird > 450) {
+    offsetThird = 0;
+  }
+  popupLine.style.left = -offsetThird + 'px';
+  pag1.classList.toggle('pag-active');
+  pag2.classList.toggle('pag-active');
+});
+
+document.querySelector('.popup__prev').addEventListener('click', function () {
+  offsetThird = offsetThird - 450;
+  if (offsetThird < 0) {
+    offsetThird = 450;
+  }
+  popupLine.style.left = -offsetThird + 'px';
+  pag1.classList.toggle('pag-active');
+  pag2.classList.toggle('pag-active');
+});
+
+logo[0].addEventListener('click', () => {
+  offset = 0;
+  containersLine.style.left = -offset + 'px';
+});
+logo[1].addEventListener('click', () => {
+  offset = 0;
+  containersLine.style.left = -offset + 'px';
+});
+logo[2].addEventListener('click', () => {
+  offset = 0;
+  containersLine.style.left = -offset + 'px';
+});
+
+firstContainerButton.addEventListener('click', () => {
+  offset = 1024;
+  containersLine.style.left = -offset + 'px';
+});
+
+popupOpen.addEventListener('click', () => {
+  popupContainer.classList.toggle('popup__open');
+});
+popupClose.addEventListener('click', () => {
+  popupContainer.classList.toggle('popup__open');
+});
+
+let x1 = null;
+let y1 = null;
+
+function handleTouchStart(event) {
+  x1 = event.touches[0].clientX;
+  y1 = event.touches[0].clientY;
+}
+
+function handleTouchMove(event) {
+  if (!x1 || !y1) {
+    return false;
+  }
+  let x2 = event.touches[0].clientX;
+  let y2 = event.touches[0].clientY;
+
+  let xDiff = x2 - x1;
+  let yDiff = y2 - y1;
+
+  if (Math.abs(xDiff) > Math.abs(yDiff)) {
+    if (xDiff > 0) {
+      console.log('right');
+      offset = offset + 1024;
+      if (offset > 2048) {
+        offset = 0;
+      }
+
+      containersLine.style.left = -offset + 'px';
+    }
+    if (xDiff < 0) {
+      console.log('left');
+      offset = offset - 1024;
+      if (offset < 0) {
+        offset = 2048;
+      }
+
+      containersLine.style.left = -offset + 'px';
+    }
+    console.log(offset);
+  }
+
+  x1 = null;
+  y1 = null;
+}
